4c208fed2301d42bf901f42132a0cca787618327
compainn <compainn@outlook.com>
2026-01-19 23:22:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = '8423596036:AAFt434DSpe_nTy0-uzbDQqU62GJDUdjq4Q' #замена
+
+FIRST_PRIZE = 10
+INVITE_PRIZE = 7
+ADMIN_ID = 7212300267
+MIN_WITHDRAW = 50
+
+REQUIRED_CHANNELS = [
+    -1003536863881,
+    #-1003563561184
+]
+
+GIFTS = {
+    'rocket': {'name': '🚀 ракета', 'price': 50, 'description': 'ракета'},
+}
